9a747aff610e85d293fa62395a1042bcba2e458a
akshay koshti <akshaykoshti97@gmail.com>
2021-05-13 12:17:54 +0530

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,7 @@
 * Copyright 2013-2021 Start Bootstrap
 * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-resume/blob/master/LICENSE)
 */
+document.body.style.zoom = 0.8
 (function ($) {
     "use strict"; // Start of use strict
 
